41bd5edfe7af6de49c920e4accb01493b8813070
surajkareppagol <surajkareppagol.dev@gmail.com>
2025-01-03 13:17:01 +0530

    📝 Update create.py
    - Add python and markdown boilerplate code

--- create.py
+++ create.py
@@ -1,9 +1,29 @@
-from os import mkdir
+from os import chdir, mkdir
 from sys import argv
 
 
 def main(path):
-    mkdir(path.replace(" ", "-"))
+    path = path.replace(" ", "-")
+
+    mkdir(path)
+
+    code = """from typing import List
+
+inputs = {1: [], 2: []}
+
+for _, input in inputs.items():
+    print(Solution().function(*input))
+"""
+
+    markdown = "# 🧩"
+
+    chdir(path)
+
+    with open("main.py", "w") as file:
+        file.write(code)
+
+    with open("READMEe.md", "w") as file:
+        file.write(markdown)
 
 
 if __name__ == "__main__":
